e779bc90f26833ee51559025168e30d79cad6691
Jamie Diprose <jamie.diprose@gmail.com>
2020-05-25 13:56:34 +1200

    Add more log statements and change log level to info

--- mag_archiver/mag_archiver.py
+++ mag_archiver/mag_archiver.py
@@ -25,6 +25,8 @@ from mag_archiver.mag import MagArchiverClient, MagState, MagDateType, MagReleas
 
 
 def main(timer: func.TimerRequest) -> None:
+    logging.info('mag_archiver function: started')
+
     # Get environment variables
     account_name = os.getenv('STORAGE_ACCOUNT_NAME')
     account_key = os.getenv('STORAGE_ACCOUNT_KEY')
@@ -41,19 +43,19 @@ def main(timer: func.TimerRequest) -> None:
     # List MAG containers in storage account
     client = MagArchiverClient(account_name=account_name, account_key=account_key)
     containers = client.list_containers(last_modified_thresh=1)
-    logging.debug(f'{client}')
-    logging.debug(f'Discovered containers: {containers}')
+    logging.info(f'{client}')
+    logging.info(f'Discovered containers: {containers}')
 
     # Update MagReleases Table based on discovered containers
     num_created, num_errors = client.update_releases(containers)
-    logging.debug(f'Update MagReleases table: num_created={num_created}, num_errors={num_errors}')
+    logging.info(f'Update MagReleases table: num_created={num_created}, num_errors={num_errors}')
 
     # List all discovered MAG releases and sort from oldest to newest based on release date
     releases: List[MagRelease] = client.list_releases(state=MagState.discovered, date_type=MagDateType.release,
                                                       reverse=False)
     num_releases = len(releases)
-    logging.debug(f'Number of discovered MAG releases: {num_releases}')
-    logging.debug(f'Discovered MAG releases: {releases}')
+    logging.info(f'Number of discovered MAG releases: {num_releases}')
+    logging.info(f'Discovered MAG releases: {releases}')
 
     # If 1 or more MAG releases was found then process the oldest one
     if len(releases) >= 1:
@@ -63,14 +65,14 @@ def main(timer: func.TimerRequest) -> None:
 
         # Update task to copying
         release.task = MagTask.copying_to_release_container
-        logging.debug(f'Update task to copying: {release}')
+        logging.info(f'Update task to copying: {release}')
         release.update()
-        logging.debug(f'Update task to copying finished: {release}')
+        logging.info(f'Update task to copying finished: {release}')
 
         # Copy source container to target container
-        logging.debug(f'Archive: {release}')
+        logging.info(f'Archive: {release}')
         release.archive(target_container, target_folder)  # TODO: how do you know if the copying failed?
-        logging.debug(f'Archive finished: {release}')
+        logging.info(f'Archive finished: {release}')
 
         # Update state to archived
         #   - set state to archived
@@ -82,14 +84,14 @@ def main(timer: func.TimerRequest) -> None:
         release.release_path = target_folder
         release.archived_date = pendulum.utcnow()
         release.task = MagTask.cleaning_up
-        logging.debug(f'Update state to archived: {release}')
+        logging.info(f'Update state to archived: {release}')
         release.update()
-        logging.debug(f'Update state to archived finished: {release}')
+        logging.info(f'Update state to archived finished: {release}')
 
         # Cleanup: delete source container
-        logging.debug(f'Cleanup: {release}')
+        logging.info(f'Cleanup: {release}')
         release.cleanup()
-        logging.debug(f'Cleanup finished: {release}')
+        logging.info(f'Cleanup finished: {release}')
 
         # Update state to done
         # - set state and task to done
@@ -97,6 +99,10 @@ def main(timer: func.TimerRequest) -> None:
         release.state = MagState.done
         release.task = MagTask.done
         release.done_date = pendulum.utcnow()
-        logging.debug(f'Update state to done: {release}')
+        logging.info(f'Update state to done: {release}')
         release.update()
-        logging.debug(f'Update state to done finished: {release}')
+        logging.info(f'Update state to done finished: {release}')
+    else:
+        logging.info(f'No discovered releases to process')
+
+    logging.info('mag_archiver function: finished')
